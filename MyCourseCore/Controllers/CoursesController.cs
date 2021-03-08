e630549c265953b7170d6edf532c75c70c7e1f6c
Tanjin Habibur <habibur.tanjin@gmail.com>
2021-03-08 22:59:03 +0100

    Add project files.

---
+++ MyCourseCore/Controllers/CoursesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyCourseCore.Controllers
+{
+    public class CoursesController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return Content("index");
+        }
+
+        public IActionResult Detail(string id)
+        {
+            return Content($"detail id : {id}");
+        }
+    }
+}

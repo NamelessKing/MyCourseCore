dd7407de4cbf01890eb3c7091e22c3e6e9ac9609
Tanjin Habibur <habibur.tanjin@gmail.com>
2021-04-21 23:02:21 +0200

    added ef core

---
+++ MyCourseCore/Models/Services/Application/EfCoreCourseService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyCourseCore.Models.Entities;
+using MyCourseCore.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCourseCore.Models.Services.Application
+{
+    public class EfCoreCourseService : ICourseService
+    {
+        public EfCoreCourseService(MyCourseDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        private MyCourseDbContext DbContext { get; }
+
+        public Task<CourseDetailViewModel> GetCourseAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<CourseViewModel>> GetCoursesAsync()
+        {
+            List<CourseViewModel> courses = await DbContext.Courses.Select(course => 
+            new CourseViewModel() { 
+                Id = course.Id,
+                Author = course.Author,
+                CurrentPrice = course.CurrentPrice,
+                FullPrice = course.FullPrice,
+                ImagePath = course.ImagePath,
+                Rating = course.Rating,
+                Title = course.Title
+            }).ToListAsync();
+
+            return courses;
+        }
+    }
+}
--- MyCourseCore/Startup.cs
+++ MyCourseCore/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MyCourseCore.Models.Entities;
 using MyCourseCore.Models.Services.Application;
 using MyCourseCore.Models.Services.Infrastructure;
 
@@ -30,8 +31,11 @@ namespace MyCourseCore
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddTransient<ICourseService,AdoNetCourseService>();
+            //services.AddTransient<ICourseService,AdoNetCourseService>();
+            services.AddTransient<ICourseService,EfCoreCourseService>();
             services.AddTransient<IDatabaseAccessor, SqliteDatabaseAccessor>();
+
+            services.AddDbContext<MyCourseDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
